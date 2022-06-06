188f65f39c6007e5f8666199a2023cb854acd8ea
natan96 <natan96bsiuno.fipp@gmail.com>
2022-06-06 00:00:15 -0300

    feat footer na página home e ajustes na página de login

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -11,6 +11,7 @@ import { AuthService } from 'src/app/core/services/auth.service';
 export class HomePageComponent implements OnInit {
 
   user: User | null = null;
+  currentDate = new Date();
 
   constructor(private router: Router, private _auth: AuthService) { }
 
